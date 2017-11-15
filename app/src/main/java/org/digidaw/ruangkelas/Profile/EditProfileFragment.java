207e5808284f1d600cdd5d813097eb98d89635aa
oktaviano <adorufus@gmail.com>
2017-11-15 16:28:21 +0700

    membuat perubahaan pada EditProfileFragment.java

--- app/src/main/java/org/digidaw/ruangkelas/Profile/EditProfileFragment.java
+++ app/src/main/java/org/digidaw/ruangkelas/Profile/EditProfileFragment.java
@@ -36,7 +36,6 @@ public class EditProfileFragment extends Fragment {
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference myRef;
     private FirebaseUser mCurrentUser;
-    private FirebaseMethods mFirebaseMethods;
     private String userID;
 
     private ImageView mSaveBtn;
@@ -45,190 +44,14 @@ public class EditProfileFragment extends Fragment {
     private EditText mUsername, mDisplayName, mWebiste, mAbout, mEmail, mPhoneNumber;
     private TextView editPhoto;
 
-    private UserSettings mUserSetting;
-
     private ProgressBar mProgresBar;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
-        mDisplayImage = (CircleImageView) view.findViewById(R.id.profile_photo);
-        mDisplayName = (EditText) view.findViewById(R.id.displayname);
-        mUsername = (EditText) view.findViewById(R.id.username);
-        mWebiste = (EditText) view.findViewById(R.id.website);
-        mAbout = (EditText) view.findViewById(R.id.bio);
-        mEmail = (EditText) view.findViewById(R.id.email);
-        mPhoneNumber = (EditText) view.findViewById(R.id.phoneNumber);
-        mFirebaseMethods = new FirebaseMethods(getActivity());
 
-        //setup profile image
-        setupProfileImage();
-
-        //seting tombol kembali
-        ImageView backArrow = (ImageView) view.findViewById(R.id.backButton);
-        backArrow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().finish();
-            }
-        });
-
-        //seting tombol save
-        ImageView saveBtn = (ImageView) view.findViewById(R.id.saveBtn);
-        saveBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveProfileSettings();
-            }
-        });
 
         return view;
-    }
-
-    private void saveProfileSettings(){
-        final String displayName = mDisplayName.getText().toString();
-        final String username = mUsername.getText().toString();
-        final String website = mWebiste.getText().toString();
-        final String bio = mAbout.getText().toString();
-        final String email = mEmail.getText().toString();
-        final long phone = Long.parseLong(mPhoneNumber.getText().toString());
-
-        //kasus 1, jika user mengubah usernamenya
-        if(!mUserSetting.getUser().getUsername().equals(username)){
-            checkIfUsernameExists(username);
-        }
-
-        //case2: if the user made a change to their email
-        if(!mUserSetting.getUser().getEmail().equals(email)){
-
-            // step1) Reauthenticate
-            //          -Confirm the password and email
-            ConfirmPasswordDialog dialog = new ConfirmPasswordDialog();
-            dialog.show(getFragmentManager(), getString(R.string.confirm_password_dialog));
-            dialog.setTargetFragment(EditProfileFragment.this, 1);
-
-
-            // step2) check if the email already is registered
-            //          -'fetchProvidersForEmail(String email)'
-            // step3) change the email
-            //          -submit the new email to the database and authentication
-        }
-
-        /**
-         * change the rest of the settings that do not require uniqueness
-         */
-        if(!mUserSetting.getSettings().getDisplay_name().equals(displayName)){
-            //update displayname
-            mFirebaseMethods.updateUserAccountSettings(displayName, null, null, 0);
-        }
-        if(!mUserSetting.getSettings().getWebsite().equals(website)){
-            //update website
-            mFirebaseMethods.updateUserAccountSettings(null, website, null, 0);
-        }
-        if(!mUserSetting.getSettings().getDescription().equals(bio)){
-            //update description
-            mFirebaseMethods.updateUserAccountSettings(null, null, bio, 0);
-        }
-        if(!mUserSetting.getSettings().getProfile_photo().equals(phone)){
-            //update phoneNumber
-            mFirebaseMethods.updateUserAccountSettings(null, null, null, phone);
-        }
-    }
-
-    private void checkIfUsernameExists(final String username){
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
-        Query query = reference
-                .child("Users")
-                .orderByChild("username")
-                .equalTo(username);
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if(!dataSnapshot.exists()){
-                    mFirebaseMethods.updateUsername(username);
-                    Toast.makeText(getActivity(), "Username diganti", Toast.LENGTH_SHORT).show();
-                }
-                for (DataSnapshot singleSnapshot: dataSnapshot.getChildren()){
-                    if(singleSnapshot.exists()){
-                        Toast.makeText(getActivity(), "Username sudah dipakai!", Toast.LENGTH_LONG).show();
-                    }
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-    }
-
-    private void setProfileWidgets(UserSettings userSettings) {
-        mUserSetting = userSettings;
-        UserAccountSettings settings = userSettings.getSettings();
-        UniversalImageLoader.setImage(settings.getProfile_photo(), mDisplayImage, null, "");
-        mDisplayName.setText(settings.getDisplay_name());
-        mUsername.setText(settings.getUsername());
-        mWebiste.setText(settings.getWebsite());
-        mAbout.setText(settings.getBio());
-        mEmail.setText(userSettings.getUser().getEmail());
-        mPhoneNumber.setText(String.valueOf(userSettings.getUser().getPhone_Number()));
-
-        editPhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), shareActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                getActivity().startActivity(intent);
-                getActivity().finish();
-            }
-        });
-    }
-
-    private void setupFirebaseAuth(){
-        mAuth = FirebaseAuth.getInstance();
-        mFirebaseDatabase = FirebaseDatabase.getInstance();
-        myRef = mFirebaseDatabase.getReference();
-        userID = mAuth.getCurrentUser().getUid();
-
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-
-                if(user != null){
-
-                }
-                else{
-
-                }
-            }
-        };
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                setProfileWidgets(mFirebaseMethods.getUserSettings(dataSnapshot));
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-
-    @Override
-    public void onStop(){
-        super.onStop();
-        if(mAuthListener != null){
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
     }
 }
