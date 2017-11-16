693304e1b9ce8d7926c8db18979e5ac8d4ef9dbf
oktaviano <adorufus@gmail.com>
2017-11-16 16:34:53 +0700

    commiting files

--- app/src/main/java/org/digidaw/ruangkelas/Home/MainActivity.java
+++ app/src/main/java/org/digidaw/ruangkelas/Home/MainActivity.java
@@ -2,6 +2,7 @@ package org.digidaw.ruangkelas.Home;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     //create firebase auth variable
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     private Context mContext = MainActivity.this;
     private static final int ACTIVITY_NUM = 0;
@@ -31,9 +33,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //instantiate the FirebaseAuth()
-        mAuth = FirebaseAuth.getInstance();
-
+        setupFirebaseAuth();
         setBottomNavBar();
         setupViewPager();
     }
@@ -42,20 +42,47 @@ public class MainActivity extends AppCompatActivity {
      * Setting firebase
      */
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        //cek jika user login, maka update Tampilan
-        FirebaseUser curUser = mAuth.getCurrentUser();
-
-        if(curUser == null){
-            sendToStart();
+    private void checkCurrentUser(FirebaseUser user){
+        if(user == null){
+            Intent intent = new Intent(mContext, StartActivity.class);
+            startActivity(intent);
         }
     }
 
-    private void sendToStart(){
-        Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
-        startActivity(startIntent);
+    private void setupFirebaseAuth(){
+        //instantiate the FirebaseAuth()
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                //periksa apakah user sudah login
+                checkCurrentUser(user);
+
+
+                if(user != null){
+                    //user telah masuk
+                }
+                else{
+                    //user keluar
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     /**
