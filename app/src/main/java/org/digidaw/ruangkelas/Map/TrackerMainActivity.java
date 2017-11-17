a26b89f1e3f7b03f98f74778868ee2bfb81cbc93
adorufus <adorufus@gmail.com>
2017-11-17 11:51:13 +0700

    fixed

--- app/src/main/java/org/digidaw/ruangkelas/Map/TrackerMainActivity.java
+++ app/src/main/java/org/digidaw/ruangkelas/Map/TrackerMainActivity.java
@@ -7,10 +7,16 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.digidaw.ruangkelas.R;
 
@@ -35,6 +41,66 @@ public class TrackerMainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Lacak Siswa");
         setSupportActionBar(toolbar);
+
+        //Firebase Setup
+        onlineRef = FirebaseDatabase.getInstance().getReference().child(".info/connected");
+        counterRef = FirebaseDatabase.getInstance().getReference("lastOnline");
+        currentUserRef = FirebaseDatabase.getInstance().getReference("lastOnline").child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        
+        setupSystem();
+        updateList();
+    }
+
+    private void updateList() {
+        adapter = new FirebaseRecyclerAdapter<User, ListOnlineViewHolder>(
+                User.class,
+                R.layout.user_layout,
+                ListOnlineViewHolder.class,
+                counterRef
+        ) {
+            @Override
+            protected void populateViewHolder(ListOnlineViewHolder viewHolder, User model, int position) {
+                viewHolder.txtUsername.setText(model.getUsername());
+            }
+        };
+
+        adapter.notifyDataSetChanged();
+        listOnline.setAdapter(adapter);
+    }
+
+    private void setupSystem() {
+        onlineRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue(Boolean.class)){
+                    currentUserRef.onDisconnect().removeValue();
+
+                    //set user yang online
+                    counterRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(new User(FirebaseAuth.getInstance().getCurrentUser().getEmail(), "Online"));
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        counterRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot:dataSnapshot.getChildren()){
+                    User user = postSnapshot.getValue(User.class);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -42,5 +108,20 @@ public class TrackerMainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu_tracker, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_join:
+                counterRef.child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                        .setValue(new User(FirebaseAuth.getInstance().getCurrentUser().getEmail(), "Online"));
+                break;
+
+            case R.id.kembali:
+                onBackPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
